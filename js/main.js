df33bb1b926ced1af7ec015c58491970a8f8ea12
oscarlundberg-sthlm <oscarlundberg@hotmail.com>
2021-07-11 17:43:18 +0200

    update

--- js/main.js
+++ js/main.js
@@ -4,30 +4,38 @@ let isLightSaberOn = false;
 const audioContext = new AudioContext();
 const oscillator = audioContext.createOscillator();
 const distortion = audioContext.createWaveShaper();
+const highPass = audioContext.createBiquadFilter();
 const filter = audioContext.createBiquadFilter();
 const gainNode = audioContext.createGain();
 
-// routing
+// Routing
 oscillator.connect(distortion);
-distortion.connect(filter);
+distortion.connect(highPass);
+highPass.connect(filter);
 filter.connect(gainNode);
 gainNode.connect(audioContext.destination);
+//
 
 gainNode.gain.setValueAtTime(0, audioContext.currentTime);
 
+// Settings
 oscillator.type = 'square';
 oscillator.frequency.setValueAtTime(92.50, audioContext.currentTime); // F#2
 oscillator.start();
 
-distortion.curve = new Float32Array([0,5]);
+distortion.curve = new Float32Array([0,1]);
 distortion.oversample = 'none';
 
+highPass.type = 'highpass';
+highPass.frequency.value = 300;
+
 filter.type = 'lowpass';
-let initFreq = 100;
+let initFreq = 400;
 filter.frequency.setValueAtTime(initFreq, audioContext.currentTime);
 
-let baseGain = 0.3;
+let baseGain = 0.1;
 let gainRatio = 1 / baseGain;
+//
 
 const handleOnOff = (e) => {
     if (isLightSaberOn) {
@@ -36,6 +44,7 @@ const handleOnOff = (e) => {
         isLightSaberOn = false;
         startStopBtn.innerHTML = 'Engage';
     } else {
+        audioContext.resume();
         gainNode.gain.setValueAtTime(baseGain, audioContext.currentTime);
         window.addEventListener('devicemotion', handleMotion);
         isLightSaberOn = true;
@@ -45,10 +54,14 @@ const handleOnOff = (e) => {
 
 let h1 = document.createElement('h1');
 document.body.appendChild(h1);
+
 const handleMotion = (e) => {
-    let motion = Math.abs(e.accelerationIncludingGravity.z) / 3;
-    filter.frequency.value = initFreq * Math.exp(motion);
-    gainNode.gain.value = baseGain * motion;
+    let motion = (Math.round(Math.abs(e.acceleration.z) * 100 ) / 100) / 20;
+    motion += 1;
+    oscillator.detune.value = motion * 70;
+    filter.frequency.value = initFreq * motion;
+    // filter.frequency.value = initFreq * Math.exp(motion);
+    gainNode.gain.value = baseGain * Math.exp(motion);
     
     h1.innerHTML = motion;
 }
